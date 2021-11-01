7852ebe3b09017eb679107efbeeed3c6a3a2c0a2
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-11-01 13:21:22 -0700

    first commit

---
+++ src/components/blurhash/blurhash-loader.js
@@ -0,0 +1,21 @@
+import { useState, useCallback } from "react"
+
+
+export const BlurHashLoader = () => {
+
+    // TODO:
+    //      [ ] Transition from Blur Hash to Image - delay 2 seconds
+
+    
+
+    return (
+        <Blurhash
+          hash="LhIg+c-$acjbBa$yobX84|ESaekB"
+          width={360}
+          height={200}
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+        />
+    )
+}
