6ac2e248764b005349d85995ea3409dc7481af02
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-11-01 14:19:41 -0700

    added blurhash loader component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css';
-import { Blurhash } from "react-blurhash";
+import { BlurHashLoader } from './components/blurhash/blurhash-loader';
+
 
 function App() {
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        Blur Hash Placeholder with Fade Transition: 
+        <br />
+        <br />
+        <BlurHashLoader
+          blurHash="LhIg+c-$acjbBa$yobX84|ESaekB" 
+          imageUrl="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fyesofcorsa.com%2Fwp-content%2Fuploads%2F2016%2F03%2F701164_france.jpg"
+        />
       </header>
     </div>
   );
--- src/components/blurhash/blurhash-loader.js
+++ src/components/blurhash/blurhash-loader.js
@@ -1,21 +1,52 @@
 import { useState, useCallback } from "react"
+import { Blurhash } from "react-blurhash"
 
 
-export const BlurHashLoader = () => {
-
-    // TODO:
-    //      [ ] Transition from Blur Hash to Image - delay 2 seconds
-
+export const BlurHashLoader = ({ imageUrl }) => {
     
+    const [opacity, setOpacity] = useState(0)
+
+    const handleImageLoaded = () => {
+        setOpacity(1)
+    }
+
+    const handleImageError = () => {
+        console.log('This image has failed to Load')
+    }
 
     return (
-        <Blurhash
-          hash="LhIg+c-$acjbBa$yobX84|ESaekB"
-          width={360}
-          height={200}
-          resolutionX={32}
-          resolutionY={32}
-          punch={1}
-        />
+        <div
+            style={{
+                height: '200px', 
+                width: '360px',
+                position: 'relative'
+            }} 
+            className="backdrop"
+        >
+            
+            <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+                <Blurhash
+                    hash="LhIg+c-$acjbBa$yobX84|ESaekB"
+                    width={'100%'}
+                    height={'100%'}
+                    resolutionX={32}
+                    resolutionY={32}
+                    punch={1}
+                />
+            </div>
+            <div style={{position: 'absolute', top: 0, width: '100%', height: '100%'}}>
+                <img 
+                    style={{ 
+                        width: '100%', 
+                        height: '100%', 
+                        opacity: opacity, 
+                        transition: '2s all ease-in-out'
+                    }} 
+                    src={imageUrl} 
+                    onLoad={handleImageLoaded} 
+                    onError={handleImageError} 
+                />
+            </div>
+        </div>
     )
 }
